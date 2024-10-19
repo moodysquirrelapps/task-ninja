7f0f1e23e23eed2959df766d22c5f47b17476630
Tam Le <tamleof1996@gmail.com>
2024-10-19 00:18:24 -0700

    Implement bar-by-bar plot

--- Task Ninja/Models/CategoriesChart.swift
+++ Task Ninja/Models/CategoriesChart.swift
@@ -12,39 +12,49 @@ import Foundation
 struct CategoryData: Identifiable {
     
     let id: Int
-    let name: String
     let sumActive: Int
     let sumDone: Int
-    let color: Color
+    let uiColor: UIColor
     
 }
 
 struct CategoriesAnalytics {
     
     let dataManager: DataManager = DataManager()
-    var categoriesAnalyticsArray: [CategoryData] = []
-    var categoriesAnalyticsMaxSumActive: Int = 0
-    var categoriesAnalyticsMaxSumDone: Int = 0
-    var categoriesAnalyticsMaxX: Int = 0
-    
-    init(categoriesAnalyticsArray: [CategoryData] = []) {
-        setup()
+    var categoriesAnalyticsArray: [(category: String, categoryData: CategoryData, categorySumMax: Int)] = []
+    var categoriesAnalyticsMaxSum: Int {
+        if categoriesAnalyticsArray.count > 0 {
+            let max1 = categoriesAnalyticsArray[0].categorySumMax
+            let max2 = categoriesAnalyticsArray[1].categorySumMax
+            return (max1 > max2) ? max1 : max2
+        } else {
+            return 0
+        }
     }
     
-    mutating func setup() {
+    init(categoriesAnalyticsArray: [(category: String, categoryData: CategoryData, categorySumMax: Int)] = []) {
+        instantiateProperties()
+    }
+    
+    mutating func instantiateProperties() {
         dataManager.readDatabaseEntityCategory()
         for (index, category) in dataManager.categoryArray!.enumerated() {
+            // Name
             dataManager.categorySelected = category
+            let newCategoryName = category.name!
+            // Active
             dataManager.readDatabaseEntityTask()
             let sumActive = dataManager.taskArray?.count ?? 0
+            // Done
             dataManager.readDatabaseEntityTask(withControl: "Done")
             let sumDone = dataManager.taskArray?.count ?? 0
-            let newCategoryData = CategoryData(id: index, name: category.name!, sumActive: sumActive, sumDone: sumDone, color: Color(UIColor(category.cellBackgroundColorHexString!)))
-            categoriesAnalyticsArray.append(newCategoryData)
-            categoriesAnalyticsMaxSumActive = (newCategoryData.sumActive > categoriesAnalyticsMaxSumActive) ? newCategoryData.sumActive : categoriesAnalyticsMaxSumActive
-            categoriesAnalyticsMaxSumDone = (newCategoryData.sumDone > categoriesAnalyticsMaxSumDone) ? newCategoryData.sumDone : categoriesAnalyticsMaxSumDone
+            // CategoryData
+            let newCategoryData = CategoryData(id: index, sumActive: sumActive, sumDone: sumDone, uiColor: UIColor(category.cellBackgroundColorHexString!))
+            // CategorySumMax
+            let newCategorySumMax = (sumActive > sumDone) ? sumActive : sumDone
+            // Append
+            categoriesAnalyticsArray.append((category: newCategoryName, categoryData: newCategoryData, categorySumMax: newCategorySumMax))
         }
-        categoriesAnalyticsMaxX = max(categoriesAnalyticsMaxSumActive, categoriesAnalyticsMaxSumDone)
     }
     
 }
@@ -59,51 +69,49 @@ struct CategoriesChart: View {
     var body: some View {
         VStack {
             if #available(iOS 16.0, *) {
-                Text("Active Tasks")
-                    .font(Font(regularFont as CTFont))
-                    .foregroundStyle(.black)
-                Chart {
-                    ForEach(categoriesAnalytics.categoriesAnalyticsArray) { categoryAnalytics in
-                        BarMark(
-                            x: .value("SumActive", categoryAnalytics.sumActive),
-                            y: .value("Name", categoryAnalytics.name)
-                        ).foregroundStyle(categoryAnalytics.color)
+                let dataArray = categoriesAnalytics.categoriesAnalyticsArray
+                Chart(dataArray, id: \.category) { category in
+                    BarMark(
+                        x: .value("Sum", category.categoryData.sumActive),
+                        y: .value("Name", category.category)
+                    )
+                    .foregroundStyle(Color(category.categoryData.uiColor.withAlphaComponent(0.50)))
+                    .position(by: .value("Status", "Active"))
+                    .annotation(position: .trailing, alignment: .center) {
+                        Text("Active: \(category.categoryData.sumActive)")
+                            .font(Font(smallFont as CTFont))
+                            .foregroundColor(.black)
+                    }
+                    BarMark(
+                        x: .value("Sum", category.categoryData.sumDone),
+                        y: .value("Name", category.category)
+                    )
+                    .foregroundStyle(Color(category.categoryData.uiColor.withAlphaComponent(1.00)))
+                    .position(by: .value("Status", "Done"))
+                    .annotation(position: .trailing, alignment: .center) {
+                        Text("Done: \(category.categoryData.sumDone)")
+                            .font(Font(smallFont as CTFont))
+                            .foregroundColor(.black)
                     }
                 }
-                .chartXAxisLabel(alignment: .center) {
-                    Text("Sum")
-                        .font(Font(smallFont as CTFont))
-                        .foregroundStyle(.black)
-                }
-                .chartXScale(domain: [0, categoriesAnalytics.categoriesAnalyticsMaxX + 1])
-                .chartXAxis {
-                    AxisMarks(values: .automatic(desiredCount: categoriesAnalytics.categoriesAnalyticsMaxX + 1)) {
-                        let value = $0.as(Int.self)!
-                        AxisValueLabel {
-                            Text("\(value)")
-                                .font(Font(smallFont as CTFont))
-                                .foregroundStyle(.black)
-                        }
-                    }
-                }
+                .chartXAxis { }
                 .chartYAxis {
                     AxisMarks {
                         let value = $0.as(String.self)!
                         AxisValueLabel {
                             Text("\(value)")
-                                .font(Font(smallFont as CTFont))
+                                .font(Font(regularFont as CTFont))
                                 .foregroundStyle(.black)
                         }
                     }
                 }
-                .padding(.all, 14.0)
             } else {
                 Text("Please upgrade to iOS 16.0 or higher.")
                     .font(Font(smallFont as CTFont))
                     .multilineTextAlignment(.center)
             }
-        }.multilineTextAlignment(.center)
-            .padding(.all, 24.0)
+        }
+        .padding(.all, 24.0)
     }
     
 }
